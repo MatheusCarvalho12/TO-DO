6bc6ad21c85bd9c5919171801df903fe3d9ffec9
MatheusCarvalho12 <mendoncacarvalhomatheus@gmail.com>
2022-11-08 22:00:55 -0300

    timer "funcionando"

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,12 +1,23 @@
 import styles from "./styles.module.css";
 import Button from "../Button";
-import { timer } from "../../utils/timer";
-
+function runTimer(actionName: string) {
+  const runTimer = setInterval(() => {
+    let counter = Number(localStorage.getItem(actionName));
+    counter++;
+    localStorage.setItem(actionName, counter.toString());
+  }, 1000);
+  return runTimer;
+}
+function stopTimer(runTimer: number) {
+  clearInterval(runTimer - 1);
+  clearInterval(runTimer);
+  return stopTimer;
+}
 export function Card(actionName: string) {
   return (
     <div className={styles.Card}>
-      <Button text={"Play"} onClick={() => timer(actionName, 1)} />
-      <Button text={"Stop"} onClick={() => timer(actionName, 0)} />
+      <Button text={"Play"} onClick={() => runTimer(actionName)} />
+      <Button text={"Stop"} onClick={() => stopTimer(runTimer(actionName))} />
     </div>
   );
 }
